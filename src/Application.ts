9b54d308fb5a009d39358b3c318e8c53a4e07abe
Vladimir Timofeev <vovkasm@gmail.com>
2019-04-07 22:43:15 +0300

    connect Api to application

--- src/Application.ts
+++ src/Application.ts
@@ -2,6 +2,7 @@ import { AppRegistry } from 'react-native'
 import { createAppContainer, createStackNavigator } from 'react-navigation'
 
 import MainScreen from 'src/Screens/MainScreen'
+import Api from './Api'
 
 export default class Application {
   private static _instance: Application
@@ -11,6 +12,12 @@ export default class Application {
       Application._instance = new Application()
     }
     return Application._instance
+  }
+
+  private _api: Api = new Api({ endpoint: 'https://api.exchangeratesapi.io/latest' })
+
+  get api(): Api {
+    return this._api
   }
 
   bootstrap() {
